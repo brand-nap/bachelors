a7557253b73829542a6c690e618465139ee2818f
Perez <brandon.perez@ucdenver.edu>
2023-09-01 10:39:39 -0600

    Completed Lab 2 for FoC Lab

---
+++ sem-1/fund-of-computing/BrandonPerez/completed-assignments/lab-2/perezBrandonLab2Part3.py
@@ -0,0 +1,18 @@
+#Brandon Perez
+#Lab 2 - Beginning work with basic Python
+
+def main():
+    print('Hello World');
+    print('My name is', 'Brandon');
+    print('Python is fun');
+    print('********************');
+    x = 4;
+    y = 15;
+    print("x",x);
+    print("y",y);
+    print("sum of x and y is", x+y);
+    print("difference of x and y is",x-y);
+    print("x multiplied by y is",x*y);
+    print("x divided by y is",x/y);
+
+main()
---
+++ sem-1/fund-of-computing/BrandonPerez/completed-assignments/lab-2/perezBrandonLab2Part4.py
@@ -0,0 +1,12 @@
+def main():
+    hourly_wage = 20
+
+    print('Annual salary is: ')
+    print(hourly_wage * 40 * 50)
+    print()
+
+    print('Monthly salary is: ')
+    print(((hourly_wage * 40 * 50) / 12))
+    print()
+
+main()
